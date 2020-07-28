81eee33d269f905b82f0188900d946c655ab9857
raulmejia <r8a8m8p8@gmail.com>
2020-07-28 16:41:04 +0200

    creating the orchester script

---
+++ Orchester.R
@@ -0,0 +1,31 @@
+# 3 group comparison 
+### loading required libraries
+#myargs <- commandArgs(trailingOnly = TRUE)
+
+### Data given by the user
+# path2yourdf <-myargs[1]
+
+##
+
+
+0
+7
+3
+5
+0
+0
+1
+2
+6
+7
+34
+39
+21
+96
+13
+0
+0
+0
+0
+0
+0
